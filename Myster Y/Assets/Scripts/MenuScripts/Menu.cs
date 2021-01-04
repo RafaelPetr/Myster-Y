a2e3da25732bbdbe352f5f377bd8534056b45132
RafaelPetr <69591645+RafaelPetr@users.noreply.github.com>
2021-01-03 21:38:12 -0300

    Main Title - Setting up basic main title
    -Menu script

---
+++ Myster Y/Assets/Scripts/MenuScripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour, ISelectHandler, IDeselectHandler {
+    private Button button;
+    public bool selected;
+
+    private void Awake() {
+        button = GetComponent<Button>();
+    }
+
+    private void Start() {
+        if (selected) {
+            button.Select();
+        }
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Interact") && selected) {
+            button.onClick.Invoke();
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        selected = true;
+    }
+
+    public void OnDeselect(BaseEventData eventData) {
+        selected = false;
+    }
+}
