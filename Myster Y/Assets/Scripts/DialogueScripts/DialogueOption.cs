2b4ae7e90eb0cd2e72d94ddf3a584643afc1baca
Rafael Petry da Silva <petryrafael29@gmail.com>
2022-01-24 23:16:51 -0300

    Recreating Project from Scratch
    -Added folders, archives and packages

--- Myster Y/Assets/Scripts/DialogueScripts/DialogueOption.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[System.Serializable]
-public class DialogueOption {
-    public string text;
-    public Dialogue linkedDialogue;
-}
--- Myster Y/Assets/Scripts/RoutineScripts/BenvindoRoutineManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BenvindoRoutineManager : CharacterRoutineManager {
-    public override void dontReallyKnowStill() {
-        //Debug.Log("Some functionallity go brrrrrr");
-    }
-}
